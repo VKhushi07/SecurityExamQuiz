76a96cec960418a9dc61060a101a9e54b005bbba
VKhushi07 <94143199+VKhushi07@users.noreply.github.com>
2022-05-09 19:22:37 -0400

    Final upload

---
+++ quiz.js
@@ -0,0 +1,484 @@
+var quiz = 
+{
+  // (A) PROPERTIES
+  // (A1) QUESTIONS & ANSWERS
+  // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
+  data: [
+  {
+    q : "1. In most office towers and apartment buildings what would cause elevators to automatically go to the lobby",
+    o : [
+      "An intrusion alarm",
+      "A fire alarm",
+      "Smoke",
+      "A terrorist attack"
+    ],
+    a : 1 // arrays start with 0, so answer is 70 meters
+  },
+  {
+    q : "2. Under the Charter of Rights and Freedoms a Security guard upon making a lawful arrest is required to advise a person why he/she is under arrest as long as they speak English.",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+  {
+    q : "3. If you were charged with an indictable offense in Canada you would be entitled to",
+    o : [
+      "Be heard by a Judge sitting alone",
+      "Be heard by a Judge and Jury",
+      "A preliminary hearing",
+      "All of the above"
+    ],
+    a : 3
+  },
+  {
+    q : "4. The OPP are",
+    o : [
+      "Provincial Police",
+      "Federal Police",
+      "Regional Police",
+      "Municipal Police"
+    ],
+    a : 0
+  },
+  {
+    q : "5. What does this symbol represent",
+    o : [
+      "Dangerously Reactive Material",
+      "Corrosive Material",
+      "Materials Causing Other Toxic Effects",
+      "Biohazardous Infectious Material"
+    ],
+    a : 3
+  },
+  {
+    q : "6. Positional asphyxia may occur when a person is being physically restrained. This condition can lead to",
+    o : [
+      "Injury",
+      "Death",
+      "Violent behavior",
+      "All of the above"
+    ],
+    a : 3
+  },
+ {
+    q : "7. A Security guard has no authority in Ontario, under the Liquor Licence Act, to arrest an individual for Failing to Leave a bar after being told to do so",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+  {
+    q : "8. Under the Trespass To Property Act which of the following reasons can you remove a person off private property as a property owner, agent or occupier",
+    o : [
+      "Failing to leave when asked to do so",
+      "Unauthorized activity",
+      "Entering the property when prohibited",
+      "You can't remove anyone only arrest"
+    ],
+    a : 3
+  },
+   {
+    q : "9. The Criminal Code is",
+    o : [
+      "Municipal Law",
+      "Provincial Law",
+      "Federal Law",
+      "All of the above"
+    ],
+    a : 2
+  },
+   {
+    q : "10. Sometimes making a false statement in court is ok providing you have the best interests of the victim in mind",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+   {
+    q : "11. What Is An Arraignment",
+    o : [
+      "Bail hearing",
+      "Reading of charges",
+      "Show cause hearing",
+      "None of the above"
+    ],
+    a : 1
+  },
+   {
+    q : "12. What section of the criminal code protects a private person making an lawful arrest",
+    o : [
+      "Sec 26",
+      "Sec 25",
+      "Sec 27",
+      "None of the above"
+    ],
+    a : 1
+  },
+  {
+    q : "13. An Indictable offence under the Criminal Code is a serious criminal offence",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "14. Under the Trespass To Property Act there are three offences under section 2. Which of the following is NOT an offence under section 2 of the act?",
+    o : [
+      "Enters on premises when entry is prohibited",
+      "A person is asked to leave a house party by the owner of the property but refuses to do so",
+      "engages in an activity on premises when the activity is prohibited",
+      "None of the above"
+    ],
+    a : 3
+  },
+   {
+    q : "15. What is the best definition of admissible evidence,",
+    o : [
+      "Relevant evidence to a proceeding that has not been excluded by a judge",
+      "Evidence that the defence wishes to have tossed",
+      "Best evidence rule",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "16. The maximum fine under the Trespass To Property Act is",
+    o : [
+      "$4000",
+      "$25,000",
+      "$2000",
+      "$100"
+    ],
+    a : 2
+  },
+   {
+    q : "17. Which of the following is an exception to the hearsay rule",
+    o : [
+      "Ex-parte testimony",
+      "Dying declaration",
+      "Best evidence rule",
+      "All of the above"
+    ],
+    a : 1
+  },
+   {
+    q : "18. Non-compliance to a lawful request or direction using verbal defiance would be defined as",
+    o : [
+      "Passive resistance",
+      "Combative",
+      "Cooperative",
+      "Passive"
+    ],
+    a : 0
+  },
+   {
+    q : "19. Which of the following would not be an example of a physical disability",
+    o : [
+      "Poor vision",
+      "Nerve damage",
+      "Dyslexia",
+      "Poor Engish language skills"
+    ],
+    a : 3
+  },
+   {
+    q : "20. When can a landlord enter a person's residence under the Residential Tenancies Act",
+    o : [
+      "By giving 24 hours notice in writing",
+      "If there is an emergency",
+      "All of the above"
+    ],
+    a : 2
+  },
+   {
+    q : "21. Workplace Violence Policy Requires If ______or more workers are regularly employed at a workplace, this policy must be in writing and posted in a conspicuous place in the workplace",
+    o : [
+      "3",
+      "4",
+      "6",
+      "10"
+    ],
+    a : 2
+  },
+   {
+    q : "22. MSDS stands for Material Safety Data Sheets",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "23. What provincial act deals with discrimination in the work place",
+    o : [
+      "Canadian Charter Of Rights And Freedoms",
+      "Human Rights Code",
+      "Occupational Health And Safety",
+      "None Of The Above"
+    ],
+    a : 1
+  },
+  {
+    q : "24. What purpose is your notebook?",
+    o : [
+      "To add information on potential suspects collected during your shift",
+      "To keep track of people, stolen property, details of persons you believe could be possible future suspects",
+      "A daily record of arrests, covert activities, during a shift",
+      "A daily record of factual events and activities during a shift"
+    ],
+    a : 3
+  },
+   {
+    q : "25. Security Guard daily and incident reports may go to several departments but ultimately the information is collected for",
+    o : [
+      "The police",
+      "The courts",
+      "The client",
+      "The site supervisor"
+    ],
+    a : 2
+  },
+  {
+    q : "26. Which of the following would not be a level of Court in Ontario",
+    o : [
+      "Ontario Court of Justice",
+      "Provincial/territorial Superior Courts",
+      "Ontario Court of Appeal",
+      "Court of Queen's Bench"
+    ],
+    a : 3
+  },
+  {
+    q : "27. Section 10 (a) of the Charter Of Rights and Freedoms (arrest or detention) relates to",
+    o : [
+      "A person needs to be advised of their right to council",
+      "A person has the right to be informed promptly of the reasons for the arrest or detention",
+      "Search or Seizure",
+      "Everyone has the right to life, liberty and security"
+    ],
+    a : 1
+  },
+  {
+    q : "28. Placing a choke hold on an individual you have arrested because you have a feeling he may escape could be excessive use of force",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+  {
+    q : "29. What does this symbol represent",
+    o : [
+      "Flammable and Combustible Materials",
+      "Oxidizing Materials",
+      "Biohazardous Infectious Material",
+      "None of the above"
+    ],
+    a : 0
+  },
+  {
+    q : "30. The term In house security means",
+    o : [
+      "The Security guard will be guarding a dwelling house",
+      "The Security guard is a direct employee of the business",
+      "The Security company is your employer but you are loaned to the business for a period of time as a permanent post",
+      "It is considered a promotion in the Security profession"
+    ],
+    a : 1
+  },
+  {
+    q : "31. What is a main objective of a Security guard patrolling a property or premises?",
+    o : [
+      "To establish a presence to deter potential offenders.",
+      "To get out of the Security office and avoid dozing off",
+      "To comply with notebook protocol making an entry every 20 minutes",
+      "To let the client see they are getting their monies worth"
+    ],
+    a : 0
+  },
+  {
+    q : "32. The Canadian Charter of Rights and Freedoms came into force on April 17 what year",
+    o : [
+      "1967",
+      "1979",
+      "1982",
+      "1983"
+    ],
+    a : 2
+  },
+  {
+    q : "33. WHMIS Class C Includes",
+    o : [
+      "Compressible Gas",
+      "Flammable and Combustible Liquids",
+      "Oxidizing Material",
+      "Poisonous and Infectious Material"
+    ],
+    a : 2
+  },
+  {
+    q : "34. On a site , how to deal with a bomb threat would be addressed in",
+    o : [
+      "Post standing orders",
+      "Emergency measures plan",
+      "Employee pension plan",
+      "None of the above"
+    ],
+    a : 1
+  },
+   {
+    q : "35. PSISA stands for Private Security Investigative Services Act. This act regulates the conduct of Security Guards",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "36. Section 8 of the Charter Of Rights And Freedoms deals with",
+    o : [
+      "Detention",
+      "Search or seizure",
+      "Arrest",
+      "Freedom of the press"
+    ],
+    a : 1
+  },
+   {
+    q : "37. Principles of procedural fairness in the Canadian legal system is referred to as",
+    o : [
+      "Due process",
+      "Legal rights",
+      "Men's Rea",
+      "High treason"
+    ],
+    a : 0
+  },
+   {
+    q : "38. 10 code for repeat message",
+    o : [
+      "10-9",
+      "10-5",
+      "10-6",
+      "10-7"
+    ],
+    a : 0
+  },
+   {
+    q : "39. When giving testimony in court a Security guard may with permission",
+    o : [
+      "Read details directly from his incident report for accuracy",
+      "Refer to his notes only to refresh his memory",
+      "Read from his notebook directly to make sure nothing is missed",
+      "None of the above"
+    ],
+    a : 1
+  },
+   {
+    q : "40. When defence council wishes to question a crown witness on the stand this is called",
+    o : [
+      "Examination in chief",
+      "Cross examination",
+      "Direct examination",
+      "Re-direct"
+    ],
+    a : 1
+  }
+  ],
+
+
+  // (A2) HTML ELEMENTS
+  hWrap: null, // HTML quiz container
+  hQn: null, // HTML question wrapper
+  hAns: null, // HTML answers wrapper
+
+  // (A3) GAME FLAGS
+  now: 0, // current question
+  score: 0, // current score
+
+  // (B) INIT QUIZ HTML
+  init: () => {
+    // (B1) WRAPPER
+    quiz.hWrap = document.getElementById("quizWrap");
+
+    // (B2) QUESTIONS SECTION
+    quiz.hQn = document.createElement("div");
+    quiz.hQn.id = "quizQn";
+    quiz.hWrap.appendChild(quiz.hQn);
+
+    // (B3) ANSWERS SECTION
+    quiz.hAns = document.createElement("div");
+    quiz.hAns.id = "quizAns";
+    quiz.hWrap.appendChild(quiz.hAns);
+
+    // (B4) GO!
+    quiz.draw();
+  },
+
+  // (C) DRAW QUESTION
+  draw: () => {
+    // (C1) QUESTION
+    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+    
+
+    // (C2) OPTIONS
+    quiz.hAns.innerHTML = "";
+    for (let i in quiz.data[quiz.now].o) {
+      let radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "quiz";
+      radio.id = "quizo" + i;
+      quiz.hAns.appendChild(radio);
+      let label = document.createElement("label");
+      label.innerHTML = quiz.data[quiz.now].o[i];
+      label.setAttribute("for", "quizo" + i);
+      label.dataset.idx = i;
+      label.addEventListener("click", () => { quiz.select(label); });
+      quiz.hAns.appendChild(label);
+    }
+  },
+
+  // (D) OPTION SELECTED
+  select: (option) => {
+    // (D1) DETACH ALL ONCLICK
+    let all = quiz.hAns.getElementsByTagName("label");
+    for (let label of all) {
+      label.removeEventListener("click", quiz.select);
+    }
+
+    // (D2) CHECK IF CORRECT
+    let correct = option.dataset.idx == quiz.data[quiz.now].a;
+    if (correct) {
+      quiz.score++;
+      option.classList.add("correct");
+    } else {
+      option.classList.add("wrong");
+    }
+
+    // (D3) NEXT QUESTION OR END GAME
+    quiz.now++;
+    setTimeout(() => {
+      if (quiz.now < quiz.data.length) 
+      { 
+        quiz.draw(); 
+      }
+      else {
+        quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
+        quiz.hAns.innerHTML = "";
+      }
+    }, 1000);
+  },
+
+  // (E) RESTART QUIZ
+  reset : () => {
+    quiz.now = 0;
+    quiz.score = 0;
+    quiz.draw();
+  }
+};
+window.addEventListener("load", quiz.init);
